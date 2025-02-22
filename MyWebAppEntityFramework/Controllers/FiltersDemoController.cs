81523ac4d3ba8f3484ff482d2e2308c457470f13
shyamabhishek <shyamabhishekkumar@gmail.com>
2025-02-22 07:57:23 +0530

    updated 22feb

---
+++ MyWebAppEntityFramework/Controllers/FiltersDemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebAppEntityFramework.Controllers
+{
+    public class FiltersDemoController : Controller
+    {
+        [HttpGet]
+        [OutputCache(Duration = 30,Location = System.Web.UI.OutputCacheLocation.Client)]
+        public ActionResult Index()
+        {
+            throw new Exception("This is my exception");
+            //return View();
+        }
+        
+        [HttpGet]
+        [OutputCache(Duration = 30, Location = System.Web.UI.OutputCacheLocation.Client)]
+        public ActionResult About()
+        {
+            throw new Exception("This is my exception");
+            //return View();
+        }
+        
+        public ActionResult TestError()
+        {
+            throw new Exception("This is my exception");
+        }
+    }
+}
--- MyWebAppEntityFramework/Global.asax.cs
+++ MyWebAppEntityFramework/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -14,6 +15,7 @@ namespace MyWebAppEntityFramework
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalFilters.Filters.Add(new HandleErrorAttribute());
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
